e164ce8844a16ff10ef3c9ee05b66679f825500d
Adilett Kabdollauly <web.developer@isker.kz>
2024-11-11 18:27:43 +0500

    6 commit

--- pages/ceatefortest.vue
+++ pages/ceatefortest.vue
@@ -6,8 +6,16 @@
       </v-col>
       <v-sheet class="mx-auto" width="300">
         <v-form disabled>
-          <v-text-field v-model="firstName" label="WEBSRF NO."></v-text-field>
-          <v-text-field v-model="date" label="WEBSRF date."></v-text-field>
+          <v-text-field
+            v-model="firstName"
+            label="WEBSRF NO."
+            outlined
+          ></v-text-field>
+          <v-text-field
+            v-model="date"
+            label="WEBSRF Date"
+            outlined
+          ></v-text-field>
         </v-form>
       </v-sheet>
     </v-row>
@@ -33,71 +41,65 @@
           <p>Organization BIN: {{ detailedUserData.orgBIN }}</p>
           <p>Cost Name: {{ detailedUserData.CostName }}</p>
           <p>Cost Code: {{ detailedUserData.CostCode }}</p>
-          <p>Position: {{ selectedUser.phone }}</p>
+          <p>Position: {{ detailedUserData.position }}</p>
         </div>
         <div v-else>
           <em>No additional information available for this user.</em>
         </div>
       </v-col>
+
       <v-col cols="6" class="text-right">
-        <div>
-          <!-- Search by Generalname -->
-          <v-autocomplete
-            v-model="selectedGeneralname"
-            :items="uniqueGeneralnames"
-            label="Search by Generalname"
-            outlined
-            @change="resetSelection"
-            required
-          ></v-autocomplete>
+        <v-autocomplete
+          v-model="selectedGeneralname"
+          :items="uniqueGeneralnames"
+          label="Search by Generalname"
+          outlined
+          @change="resetSelection"
+          required
+        ></v-autocomplete>
 
-          <!-- Search objects related to the selected Generalname -->
-          <v-autocomplete
-            v-model="selectedObject"
-            :items="filteredObjects"
-            item-text="name"
-            item-value="uid"
-            label="Search Related Objects"
-            return-object
-            outlined
-          ></v-autocomplete>
+        <v-autocomplete
+          v-model="selectedObject"
+          :items="filteredObjects"
+          item-text="name"
+          item-value="uid"
+          label="Search Related Objects"
+          return-object
+          outlined
+        ></v-autocomplete>
 
-          <!-- Display selected object details -->
-          <div v-if="selectedObject" class="object-details">
-            <h3>Selected Object Details:</h3>
-            <p><strong>Name:</strong> {{ selectedObject.name }}</p>
-            <p><strong>Code:</strong> {{ selectedObject.code }}</p>
-            <p><strong>UID:</strong> {{ selectedObject.uid }}</p>
-            <p>
-              <strong>Generalname:</strong> {{ selectedObject.Generalname }}
-            </p>
-          </div>
+        <div v-if="selectedObject" class="object-details">
+          <h3>Selected Object Details:</h3>
+          <p><strong>Name:</strong> {{ selectedObject.name }}</p>
+          <p><strong>Code:</strong> {{ selectedObject.code }}</p>
+          <p><strong>UID:</strong> {{ selectedObject.uid }}</p>
+          <p><strong>Generalname:</strong> {{ selectedObject.Generalname }}</p>
         </div>
       </v-col>
     </v-row>
+
     <v-row>
       <v-col>
         <v-sheet class="mx-auto">
           <v-form>
             <v-textarea
               v-model="Proposedsuppliers"
-              label="Предлагаемые поставщики:"
+              label="Предлагаемые поставщики"
               outlined
-              row-height="8"
               auto-grow
               clearable
             ></v-textarea>
             <v-textarea
               v-model="Area"
-              label="Участок:"
+              label="Участок"
               outlined
               auto-grow
-              row-height="8"
               clearable
             ></v-textarea>
           </v-form>
         </v-sheet>
       </v-col>
+
       <v-col>
         <v-autocomplete
           v-model="type_expenditure"
@@ -108,37 +110,29 @@
           :items="expenditureTypes"
           item-value="id"
           required
-          row-height="8"
           @change="fetchExpenditure"
         ></v-autocomplete>
-        <div
-          v-if="
-            (type_expenditure &&
-              type_expenditure.name === 'Reimbursable_by_Client') ||
-            (type_expenditure && type_expenditure.name === 'Backcharge')
-          "
-        >
+
+        <div v-if="showClientAutocomplete">
           <v-autocomplete
+            v-model="client_code"
+            :items="clientList"
             item-text="name"
+            item-value="bin"
+            label="Клиент"
             return-object
             outlined
-            :items="clientList"
-            @change="fetchClient"
-            v-model="client_code"
-            label="Клиент"
-            item-value="bin"
-            required
+            @change="fetchClientDetails"
           ></v-autocomplete>
           <v-textarea
-            v-model="Area"
-            label="Ссылочный документ:"
+            v-model="documents"
+            label="Ссылочный документ"
             outlined
             auto-grow
-            row-height="8"
             clearable
           ></v-textarea>
           <div v-if="client_code">
-            <p><strong>Selected client Details:</strong></p>
+            <p><strong>Selected Client Details:</strong></p>
             <p>Code: {{ client_code.code }}</p>
             <p>Name: {{ client_code.name }}</p>
             <p>BIN: {{ client_code.bin }}</p>
@@ -146,6 +140,7 @@
         </div>
       </v-col>
     </v-row>
+
     <v-row>
       <v-col>
         <v-text-field
@@ -164,16 +159,120 @@
         ></v-select>
       </v-col>
     </v-row>
+
     <v-row>
       <v-col>
-        <V-textarea
+        <v-textarea
           v-model="description_works"
           label="Описание работ"
           auto-grow
           outlined
+        ></v-textarea>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col col="2">
+        <v-data-table
+          v-model="selected"
+          :headers="headers"
+          :items="tableData"
+          item-value="id"
+          class="elevation-1"
+          dense
+          outlined
         >
-        </V-textarea
-      ></v-col>
+          <template v-slot:top>
+            <v-toolbar flat>
+              <v-toolbar-title>Editable Table with Date Range</v-toolbar-title>
+              <v-spacer></v-spacer>
+              <v-btn color="primary" @click="addRow">Add Row</v-btn>
+            </v-toolbar>
+          </template>
+          <template v-slot:body="{ items }">
+            <tr v-for="(item, index) in items" :key="item.id">
+              <td>
+                <v-text-field v-model="item.id" dense disabled></v-text-field>
+              </td>
+              <td>
+                <v-text-field v-model="item.percentual" dense></v-text-field>
+              </td>
+              <td>
+                <v-text-field v-model="item.label" dense></v-text-field>
+              </td>
+              <td>
+                <v-text-field v-model="item.icon" dense></v-text-field>
+              </td>
+              <td>
+                <v-menu
+                  v-model="item.startDateMenu"
+                  :close-on-content-click="false"
+                  transition="scale-transition"
+                  offset-y
+                  min-width="290px"
+                >
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-text-field
+                      v-model="item.startDate"
+                      readonly
+                      dense
+                      v-bind="attrs"
+                      v-on="on"
+                    ></v-text-field>
+                  </template>
+                  <v-date-picker
+                    outlined
+                    v-model="item.startDate"
+                    no-title
+                    scrollable
+                  >
+                    <v-spacer></v-spacer>
+                    <v-btn
+                      outlined
+                      text
+                      color="primary"
+                      @click="item.startDateMenu = false"
+                      >OK</v-btn
+                    >
+                  </v-date-picker>
+                </v-menu>
+              </td>
+              <td>
+                <v-menu
+                  v-model="item.endDateMenu"
+                  :close-on-content-click="false"
+                  transition="scale-transition"
+                  offset-y
+                  min-width="290px"
+                >
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-text-field
+                      v-model="item.endDate"
+                      readonly
+                      dense
+                      v-bind="attrs"
+                      v-on="on"
+                    ></v-text-field>
+                  </template>
+                  <v-date-picker v-model="item.endDate" no-title scrollable>
+                    <v-spacer></v-spacer>
+                    <v-btn
+                      text
+                      color="primary"
+                      @click="item.endDateMenu = false"
+                      >OK</v-btn
+                    >
+                  </v-date-picker>
+                </v-menu>
+              </td>
+              <td>
+                <v-btn icon @click="deleteRow(index)">
+                  <v-icon color="red">mdi-delete</v-icon>
+                </v-btn>
+              </td>
+            </tr>
+          </template>
+        </v-data-table>
+      </v-col>
     </v-row>
   </v-container>
 </template>
@@ -181,59 +280,136 @@
 <script>
 import axios from "axios";
 import data from "../public/data/getcostcentertree.json";
+import client from "../public/data/getclientlist.json";
+
 export default {
   data() {
     return {
+      headers: [
+        { text: "ID", value: "id", align: "start" },
+        { text: "Value", value: "percentual" },
+        { text: "Label", value: "label" },
+        { text: "Icon", value: "icon" },
+        { text: "Start Date", value: "startDate" },
+        { text: "End Date", value: "endDate" },
+        { text: "Actions", value: "actions", sortable: false },
+      ],
+      tableData: [
+        {
+          id: 1,
+          percentual: 87,
+          label: "Mandorle",
+          icon: "mdi-account-circle",
+          startDate: null,
+          endDate: null,
+          startDateMenu: false,
+          endDateMenu: false,
+        },
+      ],
+      selected: [],
+      nextId: 2,
+      date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
+        .toISOString()
+        .substr(0, 10),
+
+      modal: false,
+      menu2: false,
       firstName: "empty",
       date: "empty",
       Area: "",
+      documents: "",
       Proposedsuppliers: "",
-      selectedProjectByName: null,
-      selectedProjectByCode: null,
-      selectedProjectDetails: null,
-
       selectedUser: null,
       userList: [],
       detailedUserData: null,
       type_expenditure: null,
       expenditureTypes: [],
       client_code: null,
-      clientList: [],
-      radios: "",
+      clientList: client,
       description_works: "",
-      currency: "USD",
       reference_document: "",
       extra_project: "",
       selectedGeneralname: null,
       selectedObject: null,
-      projectList: [],
+      projectList: this.flattenData(data),
+      loading: false,
+      error: null,
     };
   },
+  computed: {
+    uniqueGeneralnames() {
+      return [...new Set(this.projectList.map((item) => item.Generalname))];
+    },
+    filteredObjects() {
+      return this.projectList.filter(
+        (item) => item.Generalname === this.selectedGeneralname
+      );
+    },
+    showClientAutocomplete() {
+      return (
+        this.type_expenditure &&
+        ["Reimbursable_by_Client", "Backcharge"].includes(
+          this.type_expenditure.name
+        )
+      );
+    },
+  },
   mounted() {
-    this.fetchProjects();
     this.fetchUsers();
     this.fetchExpenditure();
-    this.fetchClient();
   },
   methods: {
+    addRow() {
+      this.tableData.push({
+        id: this.nextId++,
+        percentual: "",
+        label: "",
+        icon: "",
+        startDate: null,
+        endDate: null,
+        startDateMenu: false,
+        endDateMenu: false,
+      });
+    },
+    deleteRow(index) {
+      this.tableData.splice(index, 1);
+    },
+
+    async fetchUsers() {
+      try {
+        this.loading = true;
+        const response = await axios.get("http://intra.isker.kz/api/v1/users");
+        this.userList = response.data.map((user) => ({
+          ...user,
+          full_name: `${user.last_name} ${user.first_name}`.trim(),
+        }));
+      } catch (error) {
+        console.error("Error fetching users:", error);
+        this.error = "Failed to load user data.";
+      } finally {
+        this.loading = false;
+      }
+    },
     async fetchExpenditure() {
       try {
         const response = await axios.get(
           "http://192.168.0.67/api/v1/websrf/getexpenditure"
         );
-        console.log("fetched data", response.data);
-        if (Array.isArray(response.data)) {
-          this.expenditureTypes = response.data.map((expenditure) => ({
-            ...expenditure,
-          }));
-        } else {
-          console.log("Unexpected data structure:", response.data);
+        this.expenditureTypes = response.data;
+      } catch (error) {
+        console.error("Error fetching expenditures:", error);
+      }
+    },
+    async handleUserSelection(user) {
+      try {
+        if (user) {
+          const response = await axios.get(
+            `http://192.168.0.67/api/v1/1c/websrf/getuser?iin=${user.username}`
+          );
+          this.detailedUserData = response.data.data || null;
         }
       } catch (error) {
-        console.error(
-          "Error fetching user data:",
-          error.response ? error.response.data : error
-        );
+        console.error("Error fetching detailed user data:", error);
       }
     },
     resetSelection() {
@@ -242,126 +418,12 @@ export default {
     flattenData(data) {
       return data.reduce((acc, item) => {
         acc.push(item);
-        if (item.items?.length) {
+        if (item.items && item.items.length) {
           acc.push(...this.flattenData(item.items));
         }
         return acc;
       }, []);
     },
-    async fetchClient() {
-      try {
-        const response = await axios.get(
-          "http://1c-main/testIvan/hs/intra/getclientlist"
-        );
-        console.log("fetched data", response.data);
-        if (Array.isArray(response.data)) {
-          this.clientList = response.data.map((client) => ({
-            ...client,
-          }));
-        } else {
-          console.log("Unexpected data structure:", response.data);
-        }
-      } catch (error) {
-        console.error(
-          "Error fetching user data:",
-          error.response ? error.response.data : error
-        );
-      }
-    },
-    async fetchUsers() {
-      try {
-        const response = await axios.get("http://intra.isker.kz/api/v1/users");
-        console.log("fetched data", response.data);
-        if (Array.isArray(response.data)) {
-          this.userList = response.data.map((user) => ({
-            ...user,
-            full_name: `${user.last_name} ${user.first_name}`.trim(),
-          }));
-        } else {
-          console.log("Unexpected data structure:", response.data);
-        }
-      } catch (error) {
-        console.error(
-          "Error fetching user data:",
-          error.response ? error.response.data : error
-        );
-      }
-    },
-    async handleUserSelection(user) {
-      if (user && user.username) {
-        try {
-          const response = await axios.get(
-            `http://192.168.0.67/api/v1/1c/websrf/getuser?iin=${user.username}`
-          );
-          console.log("Detailed user data:", response.data);
-          if (response.data && response.data.data) {
-            this.detailedUserData = response.data.data;
-          } else {
-            console.error("Unexpected detailed data structure:", response.data);
-          }
-        } catch (error) {
-          if (error.response && error.response.status === 400) {
-            console.warn("No data found for the provided IIN.");
-            this.detailedUserData = null; // Or show a specific message in the UI
-          } else {
-            console.error("Error fetching detailed user data:", error.message);
-          }
-        }
-      }
-    },
-    async fetchProjects() {
-      try {
-        const response = await axios.get(
-          "http://1c-main/testIvan/hs/intra/getcostcentertree"
-        );
-        if (Array.isArray(response.data)) {
-          this.projectList = this.flattenProjects(response.data);
-        }
-      } catch (error) {
-        console.error("Error fetching project data:", error);
-      }
-    },
-    flattenProjects(projects) {
-      // Flatten nested project data for easier access in autocompletes
-      const flatList = [];
-      function flatten(items) {
-        items.forEach((item) => {
-          flatList.push({
-            code: item.code,
-            name: item.name,
-            uid: item.uid,
-          });
-          if (item.items && item.items.length > 0) {
-            flatten(item.items);
-          }
-        });
-      }
-      flatten(projects);
-      return flatList;
-    },
-  },
-  computed: {
-    uniqueGeneralnames() {
-      return Array.from(
-        new Set(this.projectList.map((item) => item.Generalname))
-      );
-    },
-    filteredObjects() {
-      return this.projectList.filter(
-        (item) => item.Generalname === this.selectedGeneralname
-      );
-    },
-  },
-  async mounted() {
-    try {
-      this.projectList = this.flattenData(data);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
   },
 };
 </script>
-
-<style scoped>
-/* Add any custom styles here */
-</style>
