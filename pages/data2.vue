c7911ae79cb3611d5fec90d40c1f9401bfe2223b
Adilett Kabdollauly <web.developer@isker.kz>
2024-11-14 18:31:06 +0500

    8 commit

---
+++ pages/data2.vue
@@ -0,0 +1,35 @@
+<template>
+  <treeselect
+    :multiple="true"
+    :options="options"
+    :disable-branch-nodes="true"
+    v-model="value"
+    search-nested
+    :normalizer="normalizer"
+    :show-count="true"
+  />
+</template>
+
+<script>
+import Vue from "vue";
+import Vuex, { mapState, mapMutations } from "vuex";
+import generateOptions from "../public/data/getservicestree.json";
+import Treeselect from "@riophae/vue-treeselect";
+import "@riophae/vue-treeselect/dist/vue-treeselect.css";
+import service from "../public/data/getservicestree.json";
+
+export default {
+  components: { Treeselect },
+  data: () => ({
+    value: null,
+    options: service,
+    normalizer(node) {
+      return {
+        id: node.uid,
+        label: node.name,
+        children: node.items && node.items.length > 0 ? node.items : undefined,
+      };
+    },
+  }),
+};
+</script>
--- pages/date.vue
+++ pages/date.vue
@@ -1,49 +1,48 @@
-<!-- Vue SFC -->
 <template>
   <div id="app">
-    <treeselect v-model="value" :multiple="true" :options="options" />
+    <treeselect
+      v-model="value"
+      :multiple="true"
+      :options="options"
+      :normalizer="normalizer"
+      :load-options="loadOptions"
+      :show-count="true"
+    />
   </div>
 </template>
 
 <script>
-// import the component
 import Treeselect from "@riophae/vue-treeselect";
-// import the styles
 import "@riophae/vue-treeselect/dist/vue-treeselect.css";
+import service from "../public/data/getservicestree.json";
 
 export default {
-  // register the component
   components: { Treeselect },
   data() {
     return {
-      // define the default value
-      value: null,
-      // define options
-      options: [
-        {
-          id: "a",
-          label: "a",
-          children: [
-            {
-              id: "aa",
-              label: "aa",
-            },
-            {
-              id: "ab",
-              label: "ab",
-            },
-          ],
-        },
-        {
-          id: "b",
-          label: "b",
-        },
-        {
-          id: "c",
-          label: "c",
-        },
-      ],
+      value: [],
+      options: service,
     };
+  },
+  methods: {
+    normalizer(node) {
+      return {
+        id: node.uid,
+        label: node.name,
+        children:
+          node.groupe !== false && node.items && node.items.length > 0
+            ? node.items
+            : null, // Если items пуст, узел не ветка
+        // isDisabled: node.groupe === true, // Отключить узлы с groupe: true
+      };
+    },
+    async loadOptions({ action, parentNode }) {
+      if (action === "LOAD_CHILDREN_OPTIONS") {
+        // Пример: асинхронная загрузка данных
+        const children = await fetchChildren(parentNode.id); // Замените fetchChildren на свой метод
+        return children;
+      }
+    },
   },
 };
 </script>
